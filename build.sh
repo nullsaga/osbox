6da702b446f4fc219e35ffa7c7e5b59551ee270a
Vytautas <macross@nixos.lan>
2025-03-07 18:53:07 +0200

    add brave browser

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y tmux neovim fira-code-fonts golang distrobox make
+dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+dnf install -y tmux neovim fira-code-fonts golang distrobox make brave-browser
 
 # Use a COPR Example:
 #
