cd0282920b202fe7d956dff4ee3d1cf76e71246b
nullsaga <9040460+nullsaga@users.noreply.github.com>
2025-03-07 20:16:06 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,15 +10,46 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | rpm --import -
-cat <<EOF | tee /etc/yum.repos.d/google-chrome.repo
+
+#chrome 
+mkdir -p /var/opt # -p just in case it exists
+
+# Prepare alternatives directory
+mkdir -p /var/lib/alternatives
+
+# Setup repo
+cat << EOF > /etc/yum.repos.d/google-chrome.repo
 [google-chrome]
-name=Google Chrome Repository
-baseurl=https://dl.google.com/linux/chrome/rpm/stable/\$basearch
+name=google-chrome
+baseurl=https://dl.google.com/linux/chrome/rpm/stable/x86_64
 enabled=1
 gpgcheck=1
-gpgkey=https://dl.google.com/linux/linux_signing_key.pub
+repo_gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-google
 EOF
+
+# Import signing key
+curl --retry 3 --retry-delay 2 --retry-all-errors -sL \
+  -o /etc/pki/rpm-gpg/RPM-GPG-KEY-google \
+  https://dl.google.com/linux/linux_signing_key.pub
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-google
+
+# Now let's install the packages.
+rpm-ostree install google-chrome-stable
+
+# Clean up the yum repo (updates are baked into new images)
+rm /etc/yum.repos.d/google-chrome.repo -f
+
+# And then we do the hacky dance!
+mv /var/opt/google /usr/lib/google # move this over here
+
+#####
+# Register path symlink
+# We do this via tmpfiles.d so that it is created by the live system.
+cat >/usr/lib/tmpfiles.d/google.conf <<EOF
+L  /opt/google  -  -  -  -  /usr/lib/google
+EOF
+
 dnf install -y tmux neovim fira-code-fonts golang distrobox make google-chrome-stable
 
 # Use a COPR Example:
