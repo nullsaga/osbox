ad5a31fa7af4fe61ae939a67932b31fd13bed615
nullsaga <9040460+nullsaga@users.noreply.github.com>
2025-03-07 20:03:21 +0200

    attempt to install chrome

--- build.sh
+++ build.sh
@@ -10,7 +10,15 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-curl -sSL https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome.repo -o /etc/yum.repos.d/google-chrome.repo
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | rpm --import -
+cat <<EOF | tee /etc/yum.repos.d/google-chrome.repo
+[google-chrome]
+name=Google Chrome Repository
+baseurl=https://dl.google.com/linux/chrome/rpm/stable/\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://dl.google.com/linux/linux_signing_key.pub
+EOF
 dnf install -y tmux neovim fira-code-fonts golang distrobox make google-chrome-stable
 
 # Use a COPR Example:
