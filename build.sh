7419dec8bbd31ae6cf2749927235c3db0ac80518
nullsaga <9040460+nullsaga@users.noreply.github.com>
2025-03-07 19:32:32 +0200

    attempt to install brave

--- build.sh
+++ build.sh
@@ -10,8 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y  dnf-plugins-core
-dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+curl -fsSLo /etc/yum.repos.d/brave-browser.repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 dnf install -y tmux neovim fira-code-fonts golang distrobox make brave-browser
 
 # Use a COPR Example:
