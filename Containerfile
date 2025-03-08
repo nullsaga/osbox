8826f547a1ba7cb5d37f58b33058ef975a39d5b3
nullsaga <9040460+nullsaga@users.noreply.github.com>
2025-03-08 10:22:09 +0200

    restructure

--- Containerfile
+++ Containerfile
@@ -13,7 +13,7 @@ FROM ghcr.io/ublue-os/kinoite-main:latest
 ## make modifications desired in your image and install packages by modifying the build.sh script
 ## the following RUN directive does all the things required to run "build.sh" as recommended.
 
-COPY build.sh /tmp/build.sh
+COPY build_files/build.sh /tmp/build.sh
 
 RUN mkdir -p /var/lib/alternatives && \
     /tmp/build.sh && \
---
+++ build_files/brew.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Convince the installer we are in CI
+touch /.dockerenv
+
+# Make these so script will work
+mkdir -p /var/home
+mkdir -p /var/roothome
+
+# Brew Install Script
+curl --retry 3 -Lo /tmp/brew-install https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+chmod +x /tmp/brew-install
+/tmp/brew-install
+tar --zstd -cvf /usr/share/homebrew.tar.zst /home/linuxbrew/.linuxbrew
+cp -R /home/linuxbrew /usr/share/homebrew
+chown -R 1000:1000 /usr/share/homebrew
