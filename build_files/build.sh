54c1e67344228a2cc61ebf7d3b9b94db8fe40d46
nullsaga <9040460+nullsaga@users.noreply.github.com>
2025-03-15 15:13:26 +0200

    add podman compose

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf install -y tmux neovim fira-code-fonts distrobox make
+dnf install -y tmux neovim fira-code-fonts distrobox make podman-compose
 /tmp/chrome.sh
 
 # Use a COPR Example:
