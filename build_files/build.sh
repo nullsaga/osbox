fe9b760746975afbe47f9cc84279bae478d30f82
nullsaga <9040460+nullsaga@users.noreply.github.com>
2025-03-16 10:23:55 +0200

    disable docker for now

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ set -ouex pipefail
 dnf install -y neovim fira-code-fonts distrobox make podman-compose
 
 /ctx/chrome.sh
-ctx/docker-ce.sh
+#/ctx/docker-ce.sh
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
@@ -23,4 +23,4 @@ ctx/docker-ce.sh
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
-systemctl enable docker.socket
+#systemctl enable docker.socket
